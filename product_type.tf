27bb1d026e906df18a845db901e5718b34c7d561
nagarjuna3107 <40621657+nagarjuna3107@users.noreply.github.com>
2024-11-15 13:17:40 +0530

    v-15-1

--- product_type.tf
+++ product_type.tf
@@ -1,6 +1,6 @@
-resource "commercetools_product_type" "my-product-type-141124-1" {
-  key         = "my-product-type-key-141124-1"
-  name        = "Lens specification-141124-1"
+resource "commercetools_product_type" "my-product-type-151124-1" {
+  key         = "my-product-type-key-151124-1"
+  name        = "Lens specification-151124-1"
   description = "All the specific info concerning the lens"
 
   attribute {
